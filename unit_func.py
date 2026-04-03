86312a40b71dd7c95fb59e5e8992f074c4486305
Aditya-20hub <aditya.deokar@vaastav.tech>
2026-04-03 14:07:06 +0530

    <Added the new function of is_div_by_n>
    <This is the function created to know about the divisions of the given numbers>

--- unit_func.py
+++ unit_func.py
@@ -1,4 +1,19 @@
 from combination import power, fact, combination
 
+def is_divide_by_n(num: int,n: int) -> bool:
+    if num % n == o :
+        return True
+    else 
+        return False
+        
+
 def unit_func(x: int, i: int, n:int) -> float:
-    pass
+    """
+    Calculate the unit_func of x, i  and n.
+    :param x: base value.
+    :param i: ittration value.
+    :param n: number of terms.
+    :return: unit_func of x, i and n.
+    
+    """
+    if 
