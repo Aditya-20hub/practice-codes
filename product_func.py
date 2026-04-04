df9a6d406934f806910f3feb7e66b263e88d5efa
Aditya-20hub <aditya.deokar@vaastav.tech>
2026-04-04 12:39:06 +0530

    Added function defination and description to the prodect_func file

---
+++ product_func.py
@@ -0,0 +1,10 @@
+from combination import fact, power, conditional, combination
+
+def product_func(x:int) -> float:
+    
+    """
+    calculate the producte function of x.
+    
+    :param x: base values.
+    :return: product function of x
+    """
