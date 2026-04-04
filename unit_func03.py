79fdbb58ac3ec2fbee65008bc4b9e6b0fdf0cd88
Aditya-20hub <aditya.deokar@vaastav.tech>
2026-04-04 13:36:55 +0530

    Added description

--- unit_func03.py
+++ unit_func03.py
@@ -1,10 +1,21 @@
 from combination import fact, power, combination
 
-def combine_sum (x:int, n:int) -> float):
+def combine_sum (x:int, n:int) -> float:
+    """
+    calculate 1combine_sum of x and n.
+    
+    :param x: base value.
+    :param n: number of terms.
+    :return: cumbine_sum of x and n.
+    """
     retval = 0
     i = 0
     while i < n:
-        retval = retval + combination(n,n+1) * power(x,i) / fact(i)
+        retval = retval + combination(n,n) * power(x,i) / fact(i)
         i += 1
     return retval
     
+  
+  
+if __name__ == "__main__":
+        print(combine_sum(5, 8))  
