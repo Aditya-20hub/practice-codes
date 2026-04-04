4b1391f1827df68cef7c59762b966335f856ed90
Aditya-20hub <aditya.deokar@vaastav.tech>
2026-04-04 13:02:55 +0530

    i remove the if-else part cause this was comming nothing to commit, working tree clean

--- product_func.py
+++ product_func.py
@@ -8,3 +8,14 @@ def product_func(x:int) -> float:
     :param x: base values.
     :return: product function of x
     """
+    if x >= 4:
+        return f(x-1) - f(x-2) / f(x-3)
+    elif x == 3:
+        return 
+    elif x == 1:
+        return 
+    elif x == 0:
+        return 3.14159
+    else:
+    return("Invalid Input")
+    
